99b0c4023046e2452d7db188924da9214baa8c66
Jessica Parsons <jess@junipercreative.co>
2025-01-03 14:55:20 -0800

    Added CategorySectionVIew as a subview

---
+++ CampKit/Components/CategorySectionView.swift
@@ -0,0 +1,18 @@
+//
+//  CategorySectionView.swift
+//  CampKit
+//
+//  Created by Jessica Parsons on 1/3/25.
+//
+
+import SwiftUI
+
+struct CategorySectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategorySectionView()
+}
