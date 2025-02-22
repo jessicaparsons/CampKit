4dcd58225d1fb9bfd1c03875c2c1123648dacb13
Jessica Parsons <jess@junipercreative.co>
2025-02-22 08:35:59 -0800

    Add StoreKit and purchaseable upgrade to Pro

---
+++ CampKit/Screens/HomeListView/Child View/ProFeaturesListItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ProFeaturesListItemView.swift
+//  CampKit
+//
+//  Created by Jessica Parsons on 2/21/25.
+//
+
+import SwiftUI
+
+struct ProFeaturesListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProFeaturesListItemView()
+}
